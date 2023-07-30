562dae945ff8708edf3eee4e42219965b4925acd
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-30 12:28:55 -0300

    Feat: pagina de login criada sem estilização do botão

--- rickAndMortyProject/src/components/Login.tsx
+++ rickAndMortyProject/src/components/Login.tsx
@@ -1,6 +1,24 @@
 const Login = () => {
   return(
-    <p>Login page</p>
+    <>
+    <header>
+      <h1>Welcome to Rick and Mortypédia</h1>
+    </header>
+    <main>     
+      <section id='loginContainer'>
+        <img src="/src/images/loginPageImage.jpg" alt="" />
+        <form>
+          <div>
+          <h2>Login:</h2>
+          <input type="text" />
+          <h2>E-mail:</h2>
+          <input type="email" name="email" id="" />
+          </div>
+          <button>Login</button>
+        </form>        
+      </section>
+    </main>
+    </>
   )
 }
 
