15ca2a579707a6093688431f97ca94823b231735
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-30 15:36:24 -0300

    Feat: Cria cards de personagens

--- rickAndMortyProject/src/components/Home.tsx
+++ rickAndMortyProject/src/components/Home.tsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
+import { CharacterType } from '../types';
 import CharactersCard from './CharactersCard';
 
 const Home = () => {
+  const [characters, setCharacters] = useState<CharacterType[]>([]);
+  const [ pageNavigation, setPageNavigation ] = useState() 
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getApiInfo = async () => {
+      const result = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await result.json();
+      setPageNavigation(data.info);
+      setCharacters(data.results);
+      setLoading(false);  
+    }
+    getApiInfo();
+
+  }, [])
+  console.log(characters)
+  console.log(pageNavigation)
+  
   return(
-    <p>Homepage</p>
+    <main>
+      <section className='cardContainer'>
+        {loading && (
+          <h1>Loading...</h1>
+        )}
+        {!loading && (
+          characters.map((character) => <CharactersCard key={character.id} character={ character } />)
+        )}
+      </section>
+    </main>
   )
 }
 
