662c12244537140634b0cfe310a36205b583a5db
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-31 13:24:55 -0300

    Feat: layout do componente header criado, sem as funções

--- rickAndMortyProject/src/components/Header.tsx
+++ rickAndMortyProject/src/components/Header.tsx
@@ -1,7 +1,32 @@
-const Home = () => {
+import { getUser } from '../userAPI';
+import { useState } from 'react';
+import { StyledBtn } from '../style/StyledBtn.style';
+
+const Header = () => {
   return(
-    <p>header</p>
+    <header>
+      <h1 id='headerTitle'>Rick and Mortypedia</h1>
+      <section id='headerMenu'>
+        <div>
+          <h2>User</h2>
+        </div>
+        <div>
+          <h2>Link to home</h2>
+        </div>
+        <div>
+          <h2>Link to Game</h2>
+        </div>
+        <div>
+          <input type="text" name='search' />
+          <StyledBtn>Search</StyledBtn>
+        </div>
+        <div>
+          <h2>Log off</h2>
+        </div>      
+      </section>
+    </header>
+
   )
 }
 
-export default Home;
+export default Header;
