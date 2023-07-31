a25f85450c6cd1fc9c1e649346e89e99dc114c25
Gabriel Muniz <gabrielmfd@gmail.com>
2023-07-31 12:59:03 -0300

    Fix: conserto na pag NotFound onde não aparecia o loading.

--- rickAndMortyProject/src/components/NotFound.tsx
+++ rickAndMortyProject/src/components/NotFound.tsx
@@ -1,13 +1,29 @@
 import { StyledBtn } from '../style/StyledBtn.style';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Loading from './Loading';
 
 const NotFound = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+      navigate('/home');
+      setLoading(false);
+    }, 1500)   
+    
+  }
   return(
     <main>
-      <section id='notFound'>
-        <img src="/src/images/notFound.png" alt="Página não encontrada" />
-        <Link to='/home'><StyledBtn>Home</StyledBtn></Link>
-      </section>
+      {loading && <Loading />}
+      {!loading && (
+        <section id='notFound'>
+          <img src="/src/images/notFound.png" alt="Página não encontrada" />
+          <StyledBtn onClick={ handleClick }>Home</StyledBtn>
+        </section>  
+      )}
     </main>
   )
 }
