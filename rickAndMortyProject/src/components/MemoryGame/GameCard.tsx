adcbce0fd79bfbbf00525ab56d2b6baee57b80c8
Gabriel Muniz <gabrielmfd@gmail.com>
2023-08-04 09:50:01 -0300

    Fix: conserto nas funções do GameCard

--- rickAndMortyProject/src/components/MemoryGame/GameCard.tsx
+++ rickAndMortyProject/src/components/MemoryGame/GameCard.tsx
@@ -6,21 +6,31 @@ type imageURLProp = {
 
 const GameCard = ({ imageURL }: imageURLProp) => {
   const [revealFront, setRevealFront] = useState<string>('');
+  const [targetImgURL, setTargetImgURL] = useState<string>('');
+  const [compareImages, setCompareImages] = useState<string>('');
  
-  const handleClick = ({target}: React.MouseEvent<HTMLInputElement>) => { 
-    if (revealFront === '') {
-      setRevealFront('revealFront');
-      console.log(target);
-    } else {
-      setRevealFront('');
-      console.log(target);
+  const handleClick = () => { 
+    if (!revealFront) {
+      setRevealFront('revealFront');         
+    }
+    if (targetImgURL === '') {
+      setTargetImgURL(imageURL);
+    } else if (targetImgURL && compareImages === '') {
+      setCompareImages(imageURL);
     }
   }
+  
+  console.log(`targetIMAGE = ${targetImgURL}`);
+  console.log(`compareIMAGE = ${compareImages}`)
+
 
   return(
     <div className={`gameCardContainer ${revealFront}`} onClick={ handleClick }>
-      <div className='gameCard back' />
-      <img className='gameCard front ' src={ imageURL } alt="characters image" />
+      <div className='gameCard back'/>
+      <img className='gameCard front ' 
+           src={ imageURL } 
+           alt="characters image" 
+            />
     </div>
   )
 }
