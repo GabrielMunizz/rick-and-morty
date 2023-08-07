a176c3267f9355418a8d8a004b114e045107eda5
Gabriel Muniz <gabrielmfd@gmail.com>
2023-08-07 15:28:35 -0300

    Save

--- rickAndMortyProject/src/types.ts
+++ rickAndMortyProject/src/types.ts
@@ -26,13 +26,3 @@ export type UserType = {
   name: string;  
 };
 
-export const shuffleArray = (array: string[]) => {
-  const shuffle = () => Math.random() - 0.5;
-  array.sort(shuffle);
-  return array;
-}
-
-export const generateRandomID = () => {   
-  const randomId = Math.floor((Math.random() * 826) + 1);
-  return randomId;
-}
